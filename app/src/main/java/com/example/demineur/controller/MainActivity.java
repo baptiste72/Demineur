b8c7f3284a7e4745385c0694f59b391b15b0df06
Baptiste Chauvelier <baptiste72.chauvelier@gmail.com>
2022-04-09 21:15:36 +0200

    Update activity_main layout

--- app/src/main/java/com/example/demineur/controller/MainActivity.java
+++ app/src/main/java/com/example/demineur/controller/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.demineur.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.demineur.R;
 
@@ -11,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.TYPE_STATUS_BAR);
         setContentView(R.layout.activity_main);
     }
 }
